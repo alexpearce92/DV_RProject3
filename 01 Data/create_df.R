048096e6af5e59809eefbbe1a570950797f10ec3
Alex Pearce <alexpearce92@gmail.com>
2015-03-01 12:26:50 -0600

    Changed StormDetails to StormPath and updated the data sets. Each data frame should now contain all 610K rows

--- 01 Data/create_df.R
+++ 01 Data/create_df.R
@@ -1,13 +1,14 @@
-#Generate storm details
-StormDetails.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from storm_details"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+#Generate storm meta data
+StormMeta.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from storm_meta"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+
+#Generate storm path
+StormPath.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from storm_path"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
 
 #Generate storm impact
 StormImpact.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from storm_impact"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
 
-#Generate storm meta data
-StormMeta.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from storm_meta"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
-
 #Preview tables
-tbl_df(StormDetails.df)
+tbl_df(StormPath.df)
 tbl_df(StormImpact.df)
 tbl_df(StormMeta.df)
+
--- 02 Data Wrangling/deaths_per_year.R
+++ 02 Data Wrangling/deaths_per_year.R
@@ -1,8 +1,8 @@
 # Deaths according to each type of storm over time
 deaths.df <- dplyr::left_join(StormMeta.df, StormImpact.df, by = "EVENT_ID") %>%
-  mutate("INJURIES"=INJURIES_DIRECT + INJURIES_INDIRECT, "DEATHS"=DEATHS_DIRECT + DEATHS_INDIRECT) %>% 
-  select(EVENT_TYPE, MONTH_NAME, INJURIES,DEATHS) %>%
-  arrange(desc(INJURIES)) %>%
-  group_by(MONTH_NAME, EVENT_TYPE) %>%
-  summarise(sumDeaths = sum(DEATHS), sumInjuries = sum(INJURIES))
+  mutate("INJURIES"=INJURIES_DIRECT + INJURIES_INDIRECT, "DEATHS"=DEATHS_DIRECT + DEATHS_INDIRECT, "DAMAGE"=DAMAGE_PROPERTY + DAMAGE_CROPS) %>%   
+  select(EVENT_TYPE, YEAR, INJURIES,DEATHS, DAMAGE)
+  arrange(desc(YEAR)) %>%
+  '''group_by(YEAR, EVENT_TYPE) %>%'''
+  summarise(YEAR, sumDeaths = sum(DEATHS), sumInjuries = sum(INJURIES))
 deaths.df
