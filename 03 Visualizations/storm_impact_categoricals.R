8e1c12489e5e59d52332e690aaa24ca3b6dab236
Alex Pearce <alexpearce92@gmail.com>
2015-03-04 20:25:06 -0600

    Fixes to png creation. PNG files to follow once database back online

--- 03 Visualizations/storm_impact_categoricals.R
+++ 03 Visualizations/storm_impact_categoricals.R
@@ -1,27 +1,60 @@
 # Characterization of StormImpact Categoricals
 
-myplot <- function(df, x) {
+plotHistogram <- function(df, x) {
   names(df) <- c("x")
-  p <- ggplot(df, aes(x=x)) + geom_histogram(na.rm = TRUE) + scale_x_discrete(x) + theme(axis.text.x = element_text(size=6, angle=90, vjust=0.5), axis.title.x = element_text(size=20)) + labs(title=paste("Categorical Plot - ", x)) + theme(plot.title = element_text(size=40, face="bold", vjust=1, family="Times")) 
+  p <- ggplot(df, aes(x=x)) + geom_histogram(na.rm = TRUE) + scale_x_continuous(x) + theme(axis.text.x = element_text(size=7, angle=90, vjust=0.5), axis.text.y = element_text(size=10, vjust=0.5), axis.title.x = element_text(size=10), axis.title.y = element_text(size=10), plot.title = element_text(size=15, face="bold", vjust=1, family="Times")) + labs(title=paste("Measure Plot - ", x))
   p
 }
 
-StormImpactPlotList <- list()
+plotPoint <- function (df, x) {
+  g <- names(df)
+  names(df) <- c("x", "n")
+  p <- ggplot(df, aes(x=x, y=n)) + geom_point() + scale_x_discrete(x) + labs(title=paste("Categorical Plot - ", x)) + theme(axis.text.x = element_text(size=7, angle=90, vjust=0.5),  axis.text.y = element_text(size=10, vjust=0.5), axis.title.x = element_text(size=10), axis.title.y = element_text(size=10), plot.title = element_text(size=15, face="bold", vjust=1, family="Times"))
+  p
+}
+
+StormImpactHistPlotList <- list()
+StormImpactPointPlotList <- list()
+
 for (i in names(StormImpact.df)) { 
+  
+  #Categorical Columns
   if (i %in% CategoricalsStormImpact[[1]]) {
-    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from STORM_IMPACT where \\\""i"\\\" is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
-    p <- myplot(r,i)
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from STORM_IMPACT group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- plotPoint(r,i)
     print(p) 
-    StormImpactPlotList[[i]] <- p
+    StormImpactPointPlotList[[i]] <- p
+  }
+  
+  # Measure columns
+  if (i %in% CategoricalsStormImpact[[2]]) {
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from STORM_IMPACT where \\\""i"\\\" is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- plotHistogram(r,i)
+    print(p) 
+    StormImpactHistPlotList[[i]] <- p
   }
 }
 
-# Create png for StormImpact categorical plots
+# Create png for StormImpact plots
+if (length(StormImpactPointPlotList) > 0) {
+  png("./00 Doc/StormImpactCategoricals.png", width = 25, height = 25, units = "in", res = 72)
+  grid.newpage()
+  pushViewport(viewport(layout = grid.layout(1, 6)))   
+  print(StormImpactPointPlotList[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:6))
 
-if (length(StormImpactPlotList) > 0) {
-  png("../00 Doc/StormImpactCategoricals.png", width = 35, height = 50, units = "in", res = 72)
+  dev.off()
+}
+
+if (length(StormImpactHistPlotList) > 0){
+  png("./00 Doc/StormImpactMeasurables.png", width = 15, height = 15, units = "in", res = 72)
   grid.newpage()
   pushViewport(viewport(layout = grid.layout(2, 6)))   
-  print(StormImpactPlotList[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:6))
+  print(StormImpactHistPlotList[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:6))
+  print(StormImpactHistPlotList[[2]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:2))
+  print(StormImpactHistPlotList[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 3:4))
+  print(StormImpactHistPlotList[[4]], vp = viewport(layout.pos.row = 2, layout.pos.col = 5:6))
+  print(StormImpactHistPlotList[[5]], vp = viewport(layout.pos.row = 3, layout.pos.col = 1:2))
+  print(StormImpactHistPlotList[[6]], vp = viewport(layout.pos.row = 3, layout.pos.col = 3:4))
+  print(StormImpactHistPlotList[[7]], vp = viewport(layout.pos.row = 3, layout.pos.col = 5:6))
   dev.off()
 }
--- 03 Visualizations/storm_path_categoricals.R
+++ 03 Visualizations/storm_path_categoricals.R
@@ -1,33 +1,63 @@
 # Characterization of StormPath Categoricals
 
-myplot <- function(df, x) {
+plotHistogram <- function(df, x) {
   names(df) <- c("x")
-  p <- ggplot(df, aes(x=x)) + geom_histogram(na.rm = TRUE) + scale_x_discrete(x) + theme(axis.text.x = element_text(size=6, angle=90, vjust=0.5), axis.title.x = element_text(size=20)) + labs(title=paste("Categorical Plot - ", x)) + theme(plot.title = element_text(size=40, face="bold", vjust=1, family="Times")) 
-  if (x == "END_LOCATION" | x == "BEGIN_LOCATION"){
-    p <- p + scale_y_continuous(limits=c(0,100))
-  }
+  p <- ggplot(df, aes(x=x)) + geom_histogram(na.rm = TRUE) + scale_x_continuous(x) + theme(axis.text.x = element_text(size=7, angle=90, vjust=0.5), axis.text.y = element_text(size=10, vjust=0.5), axis.title.x = element_text(size=10), axis.title.y = element_text(size=10), plot.title = element_text(size=15, face="bold", vjust=1, family="Times")) + labs(title=paste("Measure Plot - ", x))
   p
 }
 
-StormPathPlotList <- list()
+plotPoint <- function (df, x) {
+  g <- names(df)
+  names(df) <- c("x", "n")
+  p <- ggplot(df, aes(x=x, y=n)) + geom_point() + scale_x_discrete(x) + labs(title=paste("Categorical Plot - ", x)) + theme(axis.text.x = element_text(size=7, angle=90, vjust=0.5),  axis.text.y = element_text(size=10, vjust=0.5), axis.title.x = element_text(size=10), axis.title.y = element_text(size=10), plot.title = element_text(size=15, face="bold", vjust=1, family="Times"))
+  p
+}
+
+StormPathHistPlotList <- list()
+StormPathPointPlotList <- list()
+
 for (i in names(StormPath.df)) { 
+  
+  #Categorical Columns
   if (i %in% CategoricalsStormPath[[1]]) {
-    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from STORM_PATH where \\\""i"\\\" is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
-    p <- myplot(r,i)
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from STORM_PATH group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- plotPoint(r,i)
     print(p) 
-    StormPathPlotList[[i]] <- p
+    StormPathPointPlotList[[i]] <- p
+  }
+  
+  # Measure columns
+  if (i %in% CategoricalsStormPath[[2]]) {
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from STORM_PATH where \\\""i"\\\" is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_aip256',PASS='orcl_aip256',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- plotHistogram(r,i)
+    print(p) 
+    StormPathHistPlotList[[i]] <- p
   }
 }
 
-# Create png for StormPath categorical plots
-
-if (length(StormPathPlotList) > 0) {
-  png("../00 Doc/StormPathCategoricals.png", width = 50, height = 70, units = "in", res = 72)
+# Create png for StormPath plots
+if (length(StormPathPointPlotList) > 0) {
+  png("./00 Doc/StormPathCategoricals.png", width = 25, height = 25, units = "in", res = 72)
   grid.newpage()
-  pushViewport(viewport(layout = grid.layout(3, 6)))   
-  print(StormPathPlotList[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:3))
-  print(StormPathPlotList[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 4:6))
-  print(StormPathPlotList[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:6, height = 0.5))
-  print(StormPathPlotList[[4]], vp = viewport(layout.pos.row = 3, layout.pos.col = 1:6, height = 0.5))
+  pushViewport(viewport(layout = grid.layout(2, 6)))   
+  print(StormPathPointPlotList[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:3))
+  print(StormPathPointPlotList[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 4:6))
+  print(StormPathPointPlotList[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:2))
+  print(StormPathPointPlotList[[4]], vp = viewport(layout.pos.row = 2, layout.pos.col = 3:4))
+  print(StormPathPointPlotList[[5]], vp = viewport(layout.pos.row = 2, layout.pos.col = 5:6))
+  dev.off()
+}
+
+if (length(StormPathHistPlotList) > 0){
+  png("./00 Doc/StormPathMeasurables.png", width = 15, height = 25, units = "in", res = 72)
+  grid.newpage()
+  pushViewport(viewport(layout = grid.layout(4, 6)))   
+  print(StormPathHistPlotList[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:6))
+  print(StormPathHistPlotList[[2]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:3))
+  print(StormPathHistPlotList[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 4:6))
+  print(StormPathHistPlotList[[4]], vp = viewport(layout.pos.row = 3, layout.pos.col = 1:3))
+  print(StormPathHistPlotList[[5]], vp = viewport(layout.pos.row = 3, layout.pos.col = 4:6))
+  print(StormPathHistPlotList[[6]], vp = viewport(layout.pos.row = 4, layout.pos.col = 1:3))
+  print(StormPathHistPlotList[[7]], vp = viewport(layout.pos.row = 4, layout.pos.col = 4:6))
   dev.off()
 }
